50ea7eafa1ab4b26b3d6370d32d47cd2007a7d70
fuskovic <fhuskovic92@gmail.com>
2023-07-08 22:13:48 -0500

    improve error handling

--- src/store/keychain_store.rs
+++ src/store/keychain_store.rs
@@ -15,17 +15,37 @@ impl KeychainStore {
     }
 
     pub fn create(mut self, kc: NewKeychain) {
-        diesel::insert_into(keychains)
+        let result = diesel::insert_into(keychains)
             .values(&kc)
-            .execute(&mut self.conn)
-            .expect("failed to insert keychain");
+            .execute(&mut self.conn);
+        
+        match result {
+            Ok(_) => { 
+                println!("successfully created {}", kc.name) 
+            },
+            Err(err) => {
+                println!("failed to create {}: {}", kc.name, err)
+            }
+        };
     }
     
     pub fn update(mut self, kc: UpdateKeychain) {
-        diesel::update(keychains.find(kc.id))
+        let result = diesel::update(keychains.find(kc.id))
             .set(&kc)
-            .execute(&mut self.conn)
-            .expect("failed to update keychain");
+            .execute(&mut self.conn);
+
+        match result {
+            Ok(rows_affected) => {
+                if rows_affected == 0 {
+                    println!("{} not found", kc.name)
+                } else {
+                    println!("successfully updated {}", kc.name)
+                }
+            },
+            Err(err) => {
+                println!("failed to update {}: {}", kc.name, err)
+            }
+        };
     }
     
     pub fn list(mut self) {
@@ -34,8 +54,20 @@ impl KeychainStore {
     }
     
     pub fn delete(mut self, _name: String) {
-        diesel::delete(keychains.filter(name.eq(_name)))
-            .execute(&mut self.conn)
-            .expect("failed to delete keychain");
+        let result = diesel::delete(keychains.filter(name.eq(&_name)))
+            .execute(&mut self.conn);
+
+        match result {
+            Ok(rows_affected) => {
+                if rows_affected == 0 {
+                    println!("{} not found", _name)
+                } else {
+                    println!("successfully deleted {}", _name)
+                }
+            },
+            Err(err) => {
+                println!("failed to delete {}: {}", _name, err)
+            }
+        };
     }
 }
